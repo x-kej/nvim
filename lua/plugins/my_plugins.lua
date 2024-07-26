4333e8c8dd7600dd7115f85f72a51cf2e91cd39e
Kevin E. Johnson <kevin@silentplanet.com>
2024-07-26 15:12:46 -0600

    add codeium plugin

--- lua/plugins/my_plugins.lua
+++ lua/plugins/my_plugins.lua
@@ -1,4 +1,13 @@
 return {
   { "folke/zen-mode.nvim" },
   { "folke/twilight.nvim" },
+  {
+    "Exafunction/codeium.vim",
+    event = "BufEnter",
+    config = function()
+      vim.keymap.set("i", "<M-a>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+    end,
+  },
 }
